c77c95c973f38c10a5dd001064fe7ee7a182fa08
jefferson <jeffer.leo93@gmail.com>
2020-04-17 11:43:00 -0600

    Formato fecha de planilla

--- wizard/planilla.py
+++ wizard/planilla.py
@@ -4,7 +4,7 @@ from openerp import models, fields, api, _
 from openerp.exceptions import UserError, ValidationError
 import time
 import base64
-import xlwt
+import xlsxwriter
 import io
 import logging
 
@@ -19,11 +19,9 @@ class rrhh_planilla_wizard(models.TransientModel):
 
     def generar(self):
         for w in self:
-            filename = "libro.xls"
-            libro = xlwt.Workbook()
-            xlwt.add_palette_colour("custom_colour", 0x21)
-            libro.set_colour_RGB(0x21, 200, 200, 200)
-            estilo = xlwt.easyxf('pattern: pattern solid, fore_colour custom_colour')
+            f = io.BytesIO()
+            libro = xlsxwriter.Workbook(f)
+            formato_fecha = libro.add_format({'num_format': 'dd/mm/yy'})
             if w.agrupado:
                 cuentas_analiticas = set([])
                 for l in w.nomina_id.slip_ids:
@@ -39,40 +37,38 @@ class rrhh_planilla_wizard(models.TransientModel):
                             cuentas_analiticas.add('Indefinido')
 
                 for i in cuentas_analiticas:
-                    hoja = libro.add_sheet(i)
+                    hoja = libro.add_worksheet(i)
 
                     xlwt.add_palette_colour("custom_colour", 0x21)
-                    libro.set_colour_RGB(0x21, 200, 200, 200)
-                    estilo = xlwt.easyxf('pattern: pattern solid, fore_colour custom_colour')
-                    hoja.write(0, 0, 'Planilla', estilo)
-                    hoja.write(0, 1, w.nomina_id.name, estilo)
-                    hoja.write(0, 2, 'Periodo', estilo)
-                    hoja.write(0, 3, str(w.nomina_id.date_start), estilo)
-                    hoja.write(0, 4, str(w.nomina_id.date_end), estilo)
+                    hoja.write(0, 0, 'Planilla')
+                    hoja.write(0, 1, w.nomina_id.name)
+                    hoja.write(0, 2, 'Periodo')
+                    hoja.write(0, 3, w.nomina_id.date_start, formato_fecha)
+                    hoja.write(0, 4, w.nomina_id.date_end, formato_fecha)
 
                     linea = 2
                     num = 1
 
-                    hoja.write(linea, 0, 'No', estilo)
-                    hoja.write(linea, 1, 'Cod. de empleado', estilo)
-                    hoja.write(linea, 2, 'Nombre de empleado', estilo)
-                    hoja.write(linea, 3, 'Fecha de ingreso', estilo)
-                    hoja.write(linea, 4, 'Puesto', estilo)
-                    hoja.write(linea, 5, 'Dias', estilo)
+                    hoja.write(linea, 0, 'No')
+                    hoja.write(linea, 1, 'Cod. de empleado')
+                    hoja.write(linea, 2, 'Nombre de empleado')
+                    hoja.write(linea, 3, 'Fecha de ingreso')
+                    hoja.write(linea, 4, 'Puesto')
+                    hoja.write(linea, 5, 'Dias')
 
                     totales = []
                     columna = 6
                     for c in w.planilla_id.columna_id:
-                        hoja.write(linea, columna, c.name, estilo)
+                        hoja.write(linea, columna, c.name)
                         columna += 1
                         totales.append(0)
                     totales.append(0)
 
-                    hoja.write(linea, columna, 'Liquido a recibir', estilo)
-                    hoja.write(linea, columna+1, 'Banco a depositar', estilo)
-                    hoja.write(linea, columna+2, 'Cuenta a depositar', estilo)
-                    hoja.write(linea, columna+3, 'Observaciones', estilo)
-                    hoja.write(linea, columna+4, 'Cuenta analítica', estilo)
+                    hoja.write(linea, columna, 'Liquido a recibir')
+                    hoja.write(linea, columna+1, 'Banco a depositar')
+                    hoja.write(linea, columna+2, 'Cuenta a depositar')
+                    hoja.write(linea, columna+3, 'Observaciones')
+                    hoja.write(linea, columna+4, 'Cuenta analítica')
                     for l in w.nomina_id.slip_ids:
                         if l.move_id and len(l.move_id.line_ids) > 0 and l.move_id.line_ids[0].analytic_account_id:
                             if l.move_id.line_ids[0].analytic_account_id.name == i:
@@ -83,7 +79,7 @@ class rrhh_planilla_wizard(models.TransientModel):
                                 hoja.write(linea, 0, num)
                                 hoja.write(linea, 1, l.employee_id.codigo_empleado)
                                 hoja.write(linea, 2, l.employee_id.name)
-                                hoja.write(linea, 3, str(l.contract_id.date_start))
+                                hoja.write(linea, 3, l.contract_id.date_start,formato_fecha)
                                 hoja.write(linea, 4, l.employee_id.job_id.name)
                                 work = -1
                                 trabajo = -1
@@ -133,7 +129,7 @@ class rrhh_planilla_wizard(models.TransientModel):
                                 hoja.write(linea, 0, num)
                                 hoja.write(linea, 1, l.employee_id.codigo_empleado)
                                 hoja.write(linea, 2, l.employee_id.name)
-                                hoja.write(linea, 3, l.contract_id.date_start)
+                                hoja.write(linea, 3, l.contract_id.date_start,formato_fecha)
                                 hoja.write(linea, 4, l.employee_id.job_id.name)
                                 work = -1
                                 trabajo = -1
@@ -182,7 +178,7 @@ class rrhh_planilla_wizard(models.TransientModel):
                                 hoja.write(linea, 0, num)
                                 hoja.write(linea, 1, l.employee_id.codigo_empleado)
                                 hoja.write(linea, 2, l.employee_id.name)
-                                hoja.write(linea, 3, l.contract_id.date_start)
+                                hoja.write(linea, 3, l.contract_id.date_start,formato_fecha)
                                 hoja.write(linea, 4, l.employee_id.job_id.name)
                                 work = -1
                                 trabajo = -1
@@ -225,41 +221,41 @@ class rrhh_planilla_wizard(models.TransientModel):
                                 num += 1
                     columna = 6
                     for t in totales:
-                        hoja.write(linea+1, columna, totales[columna-6], estilo)
+                        hoja.write(linea+1, columna, totales[columna-6])
                         columna += 1
             else:
-                hoja = libro.add_sheet('reporte')
+                hoja = libro.add_worksheet('reporte')
 
-                hoja.write(0, 0, 'Planilla', estilo)
-                hoja.write(0, 1, w.nomina_id.name, estilo)
-                hoja.write(0, 2, 'Periodo', estilo)
+                hoja.write(0, 0, 'Planilla')
+                hoja.write(0, 1, w.nomina_id.name)
+                hoja.write(0, 2, 'Periodo')
                 logging.warn(w.nomina_id.date_start)
-                hoja.write(0, 3, str(w.nomina_id.date_start), estilo)
-                hoja.write(0, 4, str(w.nomina_id.date_end), estilo)
+                hoja.write(0, 3, w.nomina_id.date_start, formato_fecha)
+                hoja.write(0, 4, w.nomina_id.date_end, formato_fecha)
 
                 linea = 2
                 num = 1
 
-                hoja.write(linea, 0, 'No', estilo)
-                hoja.write(linea, 1, 'Cod. de empleado', estilo)
-                hoja.write(linea, 2, 'Nombre de empleado', estilo)
-                hoja.write(linea, 3, 'Fecha de ingreso', estilo)
-                hoja.write(linea, 4, 'Puesto', estilo)
-                hoja.write(linea, 5, 'Dias', estilo)
+                hoja.write(linea, 0, 'No')
+                hoja.write(linea, 1, 'Cod. de empleado')
+                hoja.write(linea, 2, 'Nombre de empleado')
+                hoja.write(linea, 3, 'Fecha de ingreso')
+                hoja.write(linea, 4, 'Puesto')
+                hoja.write(linea, 5, 'Dias')
 
                 totales = []
                 columna = 6
                 for c in w.planilla_id.columna_id:
-                    hoja.write(linea, columna, c.name, estilo)
+                    hoja.write(linea, columna, c.name)
                     columna += 1
                     totales.append(0)
                 totales.append(0)
 
-                hoja.write(linea, columna, 'Liquido a recibir', estilo)
-                hoja.write(linea, columna+1, 'Banco a depositar', estilo)
-                hoja.write(linea, columna+2, 'Cuenta a depositar', estilo)
-                hoja.write(linea, columna+3, 'Observaciones', estilo)
-                hoja.write(linea, columna+4, 'Cuenta analítica', estilo)
+                hoja.write(linea, columna, 'Liquido a recibir')
+                hoja.write(linea, columna+1, 'Banco a depositar')
+                hoja.write(linea, columna+2, 'Cuenta a depositar')
+                hoja.write(linea, columna+3, 'Observaciones')
+                hoja.write(linea, columna+4, 'Cuenta analítica')
 
                 linea += 1
                 for l in w.nomina_id.slip_ids:
@@ -269,7 +265,7 @@ class rrhh_planilla_wizard(models.TransientModel):
                     hoja.write(linea, 0, num)
                     hoja.write(linea, 1, l.employee_id.codigo_empleado)
                     hoja.write(linea, 2, l.employee_id.name)
-                    hoja.write(linea, 3, str(l.contract_id.date_start))
+                    hoja.write(linea, 3, l.contract_id.date_start,formato_fecha)
                     hoja.write(linea, 4, l.employee_id.job_id.name)
                     work = -1
                     trabajo = -1
@@ -322,14 +318,12 @@ class rrhh_planilla_wizard(models.TransientModel):
 
                 columna = 6
                 for t in totales:
-                    hoja.write(linea, columna, totales[columna-6], estilo)
+                    hoja.write(linea, columna, totales[columna-6])
                     columna += 1
 
-            f = io.BytesIO()
-            libro.save(f)
+            libro.close()
             datos = base64.b64encode(f.getvalue())
-            self.archivo = datos
-            self.write({'report': self.archivo, 'name':'planilla.xls'})
+            self.write({'archivo': datos, 'name':'planilla.xls'})
             return {
                 'context': self.env.context,
                 'view_type': 'form',
