d4fff50864bb17ccc1acacfcba5b777cce5cbe31
jefferson <jeffer.leo93@gmail.com>
2020-12-10 09:20:29 -0600

    Entradas al cambiar estructura

--- wizard/hr_payroll_payslips_by_employees.py
+++ wizard/hr_payroll_payslips_by_employees.py
@@ -12,6 +12,7 @@ class HrPayslipEmployees(models.TransientModel):
 
     @api.multi
     def compute_sheet(self):
+        datos_anteriores = []
         payslips = self.env['hr.payslip']
         [data] = self.read()
         active_id = self.env.context.get('active_id')
@@ -23,7 +24,15 @@ class HrPayslipEmployees(models.TransientModel):
         if not data['employee_ids']:
             raise UserError(_("You must select employee(s) to generate payslip(s)."))
         for employee in self.env['hr.employee'].browse(data['employee_ids']):
+            if estructura:
+                slip_data_anterior = self.env['hr.payslip'].onchange_employee_id(from_date, to_date, employee.id, contract_id=False)
+                contrato_id= self.env['hr.contract'].search([('id','=',slip_data_anterior['value'].get('contract_id'))])
+                estructura_anterior = contrato_id.struct_id
+                datos_anteriores.append({'empleado': employee,'contrato': contrato_id,'estructura_anterior': estructura_anterior})
+                contrato_id.struct_id = estructura.id
+
             slip_data = self.env['hr.payslip'].onchange_employee_id(from_date, to_date, employee.id, contract_id=False)
+
             res = {
                 'employee_id': employee.id,
                 'name': slip_data['value'].get('name'),
@@ -39,4 +48,8 @@ class HrPayslipEmployees(models.TransientModel):
             logging.warn(res)
             payslips += self.env['hr.payslip'].create(res)
         payslips.compute_sheet()
+
+        if estructura and datos_anteriores:
+            for dato in datos_anteriores:
+                dato['contrato'].struct_id = dato['estructura_anterior'].id
         return {'type': 'ir.actions.act_window_close'}
