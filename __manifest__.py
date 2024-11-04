ebc7b6df3e33af155b73ae4cadc1ffe86e71fbf7
jeffrepo <jeffer.leo93@gmail.com>
2024-11-04 11:16:12 -0600

    Meses a proyectar correctos

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,7 @@
     'website': "http://www.aquih.com",
 
     'category': 'Uncategorized',
-    'version': '2.0',
+    'version': '2.1',
 
     'depends': ['base', 'hr', 'hr_contract', 'hr_payroll_account'],
 
