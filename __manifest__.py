a01fc7b4a5f218168180414950e01d6de89015fe
jeffrepo <jeffer.leo93@gmail.com>
2025-06-12 13:55:25 +0000

    Proyectar por mes sueldos e igss

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,7 @@
     'website': "http://www.aquih.com",
 
     'category': 'Uncategorized',
-    'version': '1.0',
+    'version': '1.1',
 
     'depends': ['base', 'hr', 'hr_contract', 'hr_payroll_account', 'l10n_gt_extra', 'account_followup', 'hr_holidays', 'hr_work_entry'],
 
