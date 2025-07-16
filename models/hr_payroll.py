5679d6cbc613b22812ba34052e994f13dbbbd0d2
Jefferson Silva <jefferson@computadora911.com>
2025-07-16 07:08:56 +0000

    Calculo impunible y calculo siete

--- models/hr_payroll.py
+++ models/hr_payroll.py
@@ -304,10 +304,10 @@ class HrPayslip(models.Model):
         cuota_igss = abs(self.calcular_cuota_igss(nomina))
         rubro_ingresos = sueldos + horas_extras + bonificacion_decreto + aguinaldo + bonoc + bono_productividad + otro_ingreso_afecto
         rubro_deducciones = aguinaldo_mes + bonoc_mes + abs(cuota_igss)
-        deduccion_fija = nomina.company_id.monto_deduccion_fija
-        renta_impunible = 0 if (rubro_ingresos + rubro_deducciones - deduccion_fija) < 0 else (rubro_ingresos - rubro_deducciones - deduccion_fija)
+        deduccion_fija = nomina.company_id.monto_deduccion_fija 
+        renta_impunible = 0 if (rubro_ingresos - rubro_deducciones - deduccion_fija) < 0 else (rubro_ingresos - rubro_deducciones - deduccion_fija)
         rubro_renta_cinco = 15000 if ((renta_impunible * 0.05) > 15000) else (renta_impunible * 0.05)
-        rubro_renta_siete = ((renta_impunible - 30000) * 0.07) if ((renta_impunible * 0.05) > 15000) else 0
+        rubro_renta_siete = ((renta_impunible - 300000) * 0.07) if ((renta_impunible * 0.05) > 15000) else 0
         rubro_retencion_anual = rubro_renta_cinco + rubro_renta_siete
         retencion_isr_descontado = self.calcular_retencion_isr_descontado(nomina)
         retencion_isr_descontado_total = (rubro_retencion_anual + retencion_isr_descontado) /  (meses_proyectar + 1)
