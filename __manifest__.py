d38743feb88bc467a789d3fd3ea6e61d3e8a52dd
rocketgithub <rodrigo@aquih.com>
2026-02-27 18:08:09 -0600

    Incrementar folio

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
     'author': "Rodolfo Borstcheff",
     'website': "http://www.aquih.com",
     'category': 'Uncategorized',
-    'version': '2.4',
+    'version': '2.3',
     'depends': ['base', 'hr', 'hr_contract', 'hr_payroll_account', 'l10n_gt_extra', 'account_followup', 'hr_holidays', 'hr_work_entry'],
     'data': [
         'data/rrhh_data.xml',
