c4a50947a97f7b778dbd5b94888f57774a18aa21
rocketgithub <rodrigo@aquih.com>
2026-03-12 18:12:58 -0600

    Poner el encabezado completo en cada página.

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,7 @@
     'website': "http://www.aquih.com",
 
     'category': 'Uncategorized',
-    'version': '2.3',
+    'version': '2.4',
 
     'depends': ['base', 'hr', 'hr_contract', 'hr_payroll_account', 'l10n_gt_extra', 'account_followup', 'hr_holidays', 'hr_work_entry'],
 
