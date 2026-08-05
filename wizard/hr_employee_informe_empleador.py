97a8db24538bbbba6abb6d497cb49d57ada722b3
Rodrigo Fernández <rodrigo@aquih.com>
2026-08-05 23:48:12 +0000

    Compostura de errores en informe de empleador

--- wizard/hr_employee_informe_empleador.py
+++ wizard/hr_employee_informe_empleador.py
@@ -258,8 +258,8 @@ class rrhh_informe_empleador(models.TransientModel):
                     numero_nominas_salario = 0
                     genero = ''
                     for nomina in nomina_id:
-                        nomina_anio = nomina.date_from.year
-                        nomina_mes = nomina.date_from.month
+                        nomina_anio = nomina.date_to.year
+                        nomina_mes = nomina.date_to.month
                         if w['anio'] == nomina_anio:
                             if nomina.input_line_ids:
                                 for entrada in nomina.input_line_ids:
@@ -342,9 +342,9 @@ class rrhh_informe_empleador(models.TransientModel):
                     hoja_empleado.write(fila, 23, empleado.children or '')
                     hoja_empleado.write(fila, 24, empleado.temporalidad_contrato or '')
                     hoja_empleado.write(fila, 25, empleado.tipo_contrato or '')
-                    hoja_empleado.write(fila, 26, empleado.date_start or '')
-                    hoja_empleado.write(fila, 27, empleado.fecha_reinicio_labores or '')
-                    hoja_empleado.write(fila, 28, empleado.date_end or '')
+                    hoja_empleado.write(fila, 26, empleado.date_start or '', formato_fecha)
+                    hoja_empleado.write(fila, 27, empleado.fecha_reinicio_labores or '', formato_fecha)
+                    hoja_empleado.write(fila, 28, empleado.date_end or '', formato_fecha)
                     hoja_empleado.write(fila, 29, empleado.codigo_ocupacion or '')
                     hoja_empleado.write(fila, 30, empleado.jornada_trabajo or '')
                     hoja_empleado.write(fila, 31, dias_trabajados_anual)
