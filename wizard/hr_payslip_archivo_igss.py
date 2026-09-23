e1c0ea53e560933c140adb13ad80ffda36e73d6b
Rodrigo Fernández <rodrigo@aquih.com>
2026-09-23 22:33:15 +0000

    Ordenar empleados por primera columna

--- wizard/hr_payslip_archivo_igss.py
+++ wizard/hr_payslip_archivo_igss.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.tools.misc import format_date
 import base64
+import re
 import logging
 from datetime import datetime
 
@@ -45,9 +46,9 @@ class rrhh_igss_wizard(models.TransientModel):
             for payslip_run in w.payslip_run_id:
                 for slip in payslip_run.slip_ids:
                     if slip.employee_id.id not in empleados:
-                        empleados[slip.employee_id.id] = {'empleado_id': slip.employee_id.id,'informacion': [0] * 19, 'suspension': ''}
+                        empleados[slip.employee_id.id] = {'empleado_id': slip.employee_id.id, 'informacion': [0] * 19}
 
-                    numero_liquidacion = slip.employee_id.numero_liquidacion if slip.employee_id.numero_liquidacion else ''
+                    numero_liquidacion = str(slip.employee_id.numero_liquidacion) if slip.employee_id.numero_liquidacion and re.match(r'\d+', slip.employee_id.numero_liquidacion) else '-1'
                     numero_afiliado = slip.employee_id.igss if slip.employee_id.igss else ''
                     primer_nombre = slip.employee_id.primer_nombre if slip.employee_id.primer_nombre else ''
                     segundo_nombre = slip.employee_id.segundo_nombre if slip.employee_id.segundo_nombre else ''
@@ -104,7 +105,7 @@ class rrhh_igss_wizard(models.TransientModel):
                     empleados[slip.employee_id.id]['informacion'][18] = int(dias_laborados)
 
             if empleados:
-                for empleado in empleados.values():
+                for empleado in sorted(empleados.values(), key=lambda e: int(e['informacion'][0])):
                     for dato in empleado['informacion']:
                         index = empleado['informacion'].index(dato)
                         if index != 18:
@@ -117,7 +118,8 @@ class rrhh_igss_wizard(models.TransientModel):
                     if ausencias:
                         for ausencia in ausencias:
                             if ausencia.holiday_status_id == self.env.ref('rrhh.suspension_igss') or ausencia.holiday_status_id.suspension_igss:
-                                numero_liquidacion = str(ausencia.employee_id.numero_liquidacion) if ausencia.employee_id.numero_liquidacion else ''                                fecha_inicio = str(format_date(self.env, ausencia.request_date_from, date_format="dd/MM/yyyy"))
+                                numero_liquidacion = str(ausencia.employee_id.numero_liquidacion) if ausencia.employee_id.numero_liquidacion else ''
+                                fecha_inicio = str(format_date(self.env, ausencia.request_date_from, date_format="dd/MM/yyyy"))
                                 fecha_fin = str(format_date(self.env, ausencia.request_date_to, date_format="dd/MM/yyyy"))
                                 igss = ausencia.employee_id.igss if ausencia.employee_id.igss else ""
                                 primer_nombre = ausencia.employee_id.primer_nombre if ausencia.employee_id.primer_nombre else ""
